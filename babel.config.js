1ad24009e1df2b08efe12cd89c3cbcfbb9890e72
Shubham Sunny <shubhams@techsalo.com-at-246407410337>
2024-10-14 04:54:15 +0530

    react-native-initial-setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@service': './src/service',
+          '@navigation': './src/navigation',
+          '@state': './src/state',
+          '@utils': './src/utils',
+          '@features': './src/features',
+        },
+      },
+    ],
+  ],
 };
